450c7140bde5c928b2ee8649bda80987351776b3
Elizabeth Oviedo <eo65@afsaccess2.njit.edu>
2018-02-12 23:59:25 -0500

    Initial Start of HW

---
+++ week4HW.php
@@ -0,0 +1,55 @@
+<?php
+$date = date("Y/m/d");
+$tar = date("Y/m/d");
+$year = array(0 => 2012, 1 => 396, 2 => 300, 3 => 2000, 4 => 1100, 5 => 1089);
+
+$message1 = <<<'MESSAGE1'
+The value of $date:
+MESSAGE1;
+
+$message2 = <<<'MESSAGE2'
+The value of $tar:
+MESSAGE2;
+
+$message3 = <<<'MESSAGE3'
+The value of $year:
+MESSAGE3;
+
+echo "$message1 " . $date . "<br>";
+echo "$message2 " . $tar . "<br>";
+echo "$message3 ";
+print_r($year);
+echo "<br>";
+
+if ($date > $tar) {
+ echo "The Future <br>";
+} else if ($date < $tar) {
+ echo "The Past <br>";
+} else if ($date == $tar) {
+ echo "Oops <br>";
+} else {
+ echo "THERE IS A PROBLEM <br>";
+}
+
+$DateLength = strlen($date);
+
+echo "'/' is located in positions: ";
+for( $i = 0; $i <= $DateLength; $i++ ) { 
+  $char = substr($date, $i, 1);
+  if($char === "/")
+   echo "$i  ";
+} echo "<br>";
+
+echo "There are " . str_word_count($date) . " words in the string <br>";
+
+echo "The date length is " . $DateLength . "<br>";
+
+echo ord("Datetime") . " is the ASCII value of the first character of a string Datetime. <br>";
+
+$newArray = explode("/", $date);
+
+foreach ($newArray as $key => $value) {
+ echo "$value ";
+} echo "<br>";
+
+?>
